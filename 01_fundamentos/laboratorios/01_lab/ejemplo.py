bdd3f575d5e2837963df3a555e1db592dbc55a6f
heritaco <heriberto.espinomo@udlap.mx>
2025-08-19 18:07:24 -0600

    bonito

--- 01_fundamentos/laboratorios/01_lab/ejemplo.py
+++ 01_fundamentos/laboratorios/01_lab/ejemplo.py
@@ -1,8 +1,7 @@
+# %%
+from neurona import *
 
-#%%
-from Lab1_Torres_Arroyo_Espino_V1 import *
-
-#%%
+# %%
 neurona = AND()
 print("AND:")
 print("0 AND 0 =", neurona.Activacion(neurona.ProdVec([0, 0])))
@@ -10,6 +9,7 @@ print("0 AND 1 =", neurona.Activacion(neurona.ProdVec([0, 1])))
 print("1 AND 0 =", neurona.Activacion(neurona.ProdVec([1, 0])))
 print("1 AND 1 =", neurona.Activacion(neurona.ProdVec([1, 1])))
 
+# %% 
 neurona = OR()
 print("\nOR:")
 print("0 OR 0 =", neurona.Activacion(neurona.ProdVec([0, 0])))
@@ -17,15 +17,14 @@ print("0 OR 1 =", neurona.Activacion(neurona.ProdVec([0, 1])))
 print("1 OR 0 =", neurona.Activacion(neurona.ProdVec([1, 0])))
 print("1 OR 1 =", neurona.Activacion(neurona.ProdVec([1, 1])))
 
+# %%
 neurona = NOT()
 print("\nNOT:")
 print("NOT 0 =", neurona.Activacion(neurona.ProdVec([0])))
 print("NOT 1 =", neurona.Activacion(neurona.ProdVec([1])))
 
+# %%
 neurona = IDENTITY()
 print("\nIDENTITY:")
 print("IDENTITY 0 =", neurona.Activacion(neurona.ProdVec([0])))
 print("IDENTITY 1 =", neurona.Activacion(neurona.ProdVec([1])))
-
-# %%
-
